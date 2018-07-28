079c4896fb5af8cea076b2608618fe80359fd144
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-28 08:41:20 +0700

    Delete listexcode.js

--- listexcode.js
+++
@@ -1,7 +0,0 @@
-exports.excode = 
-    {
-           list1 : ["SendMsgAllChDiscord","view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/SendMsgToAllChDis.js"],
-           list2 : ["GetDatainArray","view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/GetDatainArray.js"]
-    }
-
-  
