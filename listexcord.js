1c7b106975ed4d7244d13dc4474d308c1f4b3f73
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-28 08:14:28 +0700

    Update and rename listexcord.json to listexcord.js

---
+++ listexcord.js
@@ -0,0 +1,5 @@
+exports.namecode = ["SendMsgAllChDiscord",
+                    "GetDatainArray"]
+exports.excode =["view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/SendMsgToAllChDis.js",
+                 "view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/GetDatainArray.js]
+   
