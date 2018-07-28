cad9eb547dd32556e4390e19ae5849b3d87edadf
yoterz <40376255+yoterz@users.noreply.github.com>
2018-07-28 08:32:33 +0700

    Update listexcode.js

--- listexcode.js
+++ listexcode.js
@@ -1,12 +1,7 @@
-exports.namecode = 
+exports.excode = 
     {
-           name : ["SendMsgAllChDiscord",
-                    "GetDatainArray"]
+           list1 : ["SendMsgAllChDiscord","view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/SendMsgToAllChDis.js"],
+           list2 : ["GetDatainArray","view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/GetDatainArray.js"]
     }
 
-exports.excode =
-    {
-            code : ["view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/SendMsgToAllChDis.js",
-                 "view-source:https://raw.githubusercontent.com/yoterz/ExampleCodeNodeJs/master/GetDatainArray.js"]
-    }
   
